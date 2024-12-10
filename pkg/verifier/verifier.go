3eb662f44c8aebc1de72e4ed582803674854ebc3
Noviiich <nowikoff63@gmail.com>
2024-12-10 19:21:13 +0300

    create function for valid email

--- pkg/verifier/verifier.go
+++ pkg/verifier/verifier.go
@@ -5,13 +5,19 @@ import (
 	"regexp"
 )
 
-const emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+func isAddressValid(email string) bool {
+	return emailRegex.MatchString(email)
+}
+
+const emailRegexString = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+
+var emailRegex = regexp.MustCompile(emailRegexString)
 
 func VerifyEmail(email string) (bool, error) {
 	if email == "" {
 		return false, errors.New("email is empty")
 	}
 
-	re := regexp.MustCompile(emailRegex)
-	return re.MatchString(email), nil
+	isAddressValid := isAddressValid(email)
+	return isAddressValid, nil
 }
